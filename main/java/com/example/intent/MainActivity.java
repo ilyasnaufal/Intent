36edc35402c256c3cfb3dd40520d3c9cbfd0f808
ilyasnaufal <75098363+ilyasnaufal@users.noreply.github.com>
2021-10-14 18:46:46 +0700

    Add files via upload

---
+++ main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMove = findViewById(R.id.btnMove);
+        Button btnShare = findViewById(R.id.btnShare);
+
+        btnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DetailActivity.class));
+            }
+        });
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                // membawa data/pesan yang ingin di share
+                intent.putExtra(intent.EXTRA_TEXT, "Hallo..." +
+                        "Saya share ke media sosial yaaa");
+                intent.setType("text/plain");
+
+                startActivity(intent.createChooser(intent, "Share to :"));
+            }
+        });
+    }
+}
